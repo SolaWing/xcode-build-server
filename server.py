85a5f360966293fffa3419b1b5086fc9e0d56c57
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2026-01-15 11:31:27 +0200

    add SourcePackages/checkout folder to targets so swift package dependencies are available for autocomplete

--- server.py
+++ server.py
@@ -460,17 +460,28 @@ def server_api():
         items = []
         for target in params["targets"]:
             if target["uri"] == "dummy://dummy":
-                items.append({
-                    "target": target,
-                    "sources": [
-                        {
-                            # TODO: 换成真实的target和sources
-                            "uri": Path(shared_state.root_path).as_uri(),
-                            "kind": 2, # 1: file, 2: directory
-                            "generated": False
-                        }
-                    ]
-                })
+                items.append(
+                    {
+                        "target": target,
+                        "sources": [
+                            {
+                                # TODO: 换成真实的target和sources
+                                "uri": Path(shared_state.root_path).as_uri(),
+                                "kind": 2,  # 1: file, 2: directory
+                                "generated": False,
+                            },
+                            {
+                                "uri": Path(
+                                    shared_state.config.build_root,
+                                    "SourcePackages",
+                                    "checkouts",
+                                ).as_uri(),
+                                "kind": 2,  # 1: file, 2: directory
+                                "generated": False,
+                            },
+                        ],
+                    }
+                )
 
         return {"jsonrpc": "2.0", "id": message["id"], "result": {"items": items }}
 
